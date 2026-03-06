ddf7430792e354dc1cf18f51bdabea76fd38ddf2
Karas02000 <gta5369577@naver.com>
2026-03-06 11:10:48 +0900

    2026/03/06 | 11:10

--- statetask/src/Users.jsx
+++ statetask/src/Users.jsx
@@ -17,17 +17,17 @@ export default function Users() {
     }
 
   return(
-    <>
-            <label>Username</label> <br />
-            <input type="text" name="username" onChange={handleChange} value={user.username} /> <br />
+    <form onSubmit={handleSubmit}>
+        <label>Username</label> <br />
+        <input type="text" name="username" onChange={handleChange} value={user.username} /> <br />
 
-            <label>Password</label> <br />
-            <input type="password" name="password" onChange={handleChange} value={user.password} /> <br />
+        <label>Password</label> <br />
+        <input type="password" name="password" onChange={handleChange} value={user.password} /> <br />
 
-            <label>E-mail</label> <br />
-            <input type="email" name="email" onChange={handleChange} value={user.email} /> <br />
+        <label>E-mail</label> <br />
+        <input type="email" name="email" onChange={handleChange} value={user.email} /> <br />
 
-            <input type="submit" value="Submit" onClick={handleSubmit} />
-        </>
+        <input type="submit" value="Submit" />
+    </form>
   );
 }
